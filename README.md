400229209eb3c1d7d27b3d6e9d1ba6cc429b69cf
Ubuntu <simon.granstrom@gmail.com>
2024-06-03 20:45:12 +0200

    fixed script

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Boat-Airplay
 Download and install shairport-sync airplay reciever.
+Pair and trust the device before running this script.
 This will automatically connect to the specified bluetooth mac-address.
 Run this on startup.
+
+# Config
+Set the mac address at the top of `bluetooth_connector.py`.
+
+# Auto-run
+copy bluetooth_connector.service to `/etc/systemd/system/`, then run:
+
+* `sudo systemctl start bluetooth_connector.service` to run the script now.
+* `sudo systemctl enable bluetooth_connector.service` to set the script to run every boot.
+
+### Notes
+https://unix.stackexchange.com/questions/334386/how-to-set-up-automatic-connection-of-bluetooth-headset
--- bluetooth_connector.py
+++ bluetooth_connector.py
@@ -1,46 +1,59 @@
 import subprocess
 import time
 
-mac_address = ""
+mac_address = "04:FE:A1:71:A7:57"
+print_logs = True
+
+def turn_on_bluetooth() -> None:
+    off = True
+    while off:
+        # Turn on bluetooth
+        ret = subprocess.run(["bluetoothctl", "power", "on"], shell=False, capture_output=True)
+
+        if "succeeded" in ret.stdout.decode():
+            off = False
+
+            # Turn off discoverability
+            subprocess.run(["bluetoothctl", "discoverable", "off"], shell=False, capture_output=True)
+        else:
+            time.sleep(1)
 
 def connect() -> bool:
-    # Turn on bluetooth
-    subprocess.call(["bluetoothctl", "power", "on"], shell=True)
-
-    # Pair device
-    ret = subprocess.run(["bluetoothctl", "pair", mac_address], shell=True)
-    if "Paired: yes" not in ret.stdout:
-        print("Could not pair to device!")
-        return False
-    
-    # Trust device
-    subprocess.call(["bluetoothctl", "trust", mac_address], shell=True)
-
     # Connect device
-    ret = subprocess.run(["bluetoothctl", "connect", mac_address], shell=True)
-    if "Connection successful" not in ret.stdout:
-        print("Could not connect to device!")
+    ret = subprocess.run(["bluetoothctl", "connect", mac_address], shell=False, capture_output=True)
+
+    if "Connection successful" not in ret.stdout.decode():
+        if print_logs:
+            print("Could not connect to device!", ret.stdout.decode())
         return False
+
+    if print_logs:
+        print("Connected!")
     
     return True
 
 
 def check_connection() -> bool:
     # Check if we are connected
-    ret = subprocess.run(["bluetoothctl", "paired-devices"])
-    connected = mac_address in ret.stdout
+    ret = subprocess.run(["bluetoothctl", "devices", "Connected"], shell=False, capture_output=True)
+    connected = mac_address in ret.stdout.decode()
     return connected
 
 
 def loop() -> None:
     while True:
+        turn_on_bluetooth()
         connected = check_connection()
 
         if not connected:
             connected = connect()
 
-        if not connected:
-            time.sleep(5)
+        if connected and print_logs:
+            print("Still connected!")
+        elif print_logs:
+            print("Disconnected!")
+
+        time.sleep(5)
 
 
 if __name__ == "__main__":
