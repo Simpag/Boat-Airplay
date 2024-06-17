1090890d450d3c413c7df42e5e8473923c427d17
MyNameIsSimon <simon.granstrom@gmail.com>
2024-06-17 03:35:54 +0200

    Update bluetooth_connector.py

--- bluetooth_connector.py
+++ bluetooth_connector.py
@@ -1,8 +1,9 @@
 import subprocess
 import time
+import re
 
-mac_address = "04:FE:A1:71:A7:57"
-print_logs = True
+#mac_address = "00:6F:F2:C6:C7:00"
+print_logs = False
 
 def turn_on_bluetooth() -> None:
     off = True
@@ -18,35 +19,59 @@ def turn_on_bluetooth() -> None:
         else:
             time.sleep(1)
 
-def connect() -> bool:
-    # Connect device
-    ret = subprocess.run(["bluetoothctl", "connect", mac_address], shell=False, capture_output=True)
 
-    if "Connection successful" not in ret.stdout.decode():
-        if print_logs:
-            print("Could not connect to device!", ret.stdout.decode())
-        return False
+def get_mac_addresses() -> list:
+    # Regular expression used to grab the mac addresses
+    re_string = re.compile(r"(?:[0-9a-fA-F]:?){12}")
+
+    # Grab all of the Paired devices
+    ret = subprocess.run(["bluetoothctl", "devices", "Paired"], shell=False, capture_output=True)
+
+    # Run the regex on the output
+    macs = ret.stdout.decode()
+    macs = re.findall(re_string, macs)
 
     if print_logs:
-        print("Connected!")
-    
-    return True
+        print("MAC addresses found: ", macs)
+
+    return macs
+
+
+def connect(addresses: list) -> bool:
+    # Try to connect to any of the devices given
+    for address in addresses:
+        ret = subprocess.run(["bluetoothctl", "connect", address], shell=False, capture_output=True)
+
+        if "Connection successful" not in ret.stdout.decode():
+            if print_logs:
+                print("Could not connect to device: ", address, " stdout: ", ret.stdout.decode())
+            continue
+
+        if print_logs:
+            print("Connected to: ", address)
+
+        return True
+
+    return False
 
 
 def check_connection() -> bool:
     # Check if we are connected
     ret = subprocess.run(["bluetoothctl", "devices", "Connected"], shell=False, capture_output=True)
-    connected = mac_address in ret.stdout.decode()
+    #connected = mac_address in ret.stdout.decode()
+    connected = len(ret.stdout) > 0
     return connected
 
 
 def loop() -> None:
+    turn_on_bluetooth()
+    addresses = get_mac_addresses()
+
     while True:
-        turn_on_bluetooth()
         connected = check_connection()
 
         if not connected:
-            connected = connect()
+            connected = connect(addresses)
 
         if connected and print_logs:
             print("Still connected!")
@@ -58,3 +83,4 @@ def loop() -> None:
 
 if __name__ == "__main__":
     loop()
+    
