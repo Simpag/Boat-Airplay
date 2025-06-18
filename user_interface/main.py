0eba8b470bc03a9f4c7bb33260db754e04b216bc
MyNameIsSimon <simon.granstrom@gmail.com>
2025-06-18 16:37:39 +0200

    removed hotspot creation from main flask app

--- user_interface/main.py
+++ user_interface/main.py
@@ -183,38 +183,6 @@ def index():
     return render_template("index.html")
 
 
-def setup_hotspot():
-    connections = nmcli.connection()
-    connections = [n.name for n in connections if n.conn_type == "wifi"]
-
-    if "RPI-Hotspot" in connections:
-        print("Hotspot already set up, skipping")
-        return
-
-    print("Setting up hotspot...")
-    nmcli.device.wifi_hotspot(
-        ssid="AirplayBridge",
-        password="12345678",
-        band="bg",
-        con_name="RPI-Hotspot",
-    )
-
-    nmcli.connection.modify(
-        "RPI-Hotspot",
-        {
-            "ipv4.addresses": "10.0.10.1/24",
-            "ipv4.method": "manual",
-            "ipv4.gateway": "",
-            "ipv4.dns": "",
-            "connection.autoconnect": "yes",
-            "connection.autoconnect-priority": "-1",
-        },
-    )
-
-    nmcli.connection.down("RPI-Hotspot")  # Apply new settings
-
-
 if __name__ == "__main__":
     # Start the connection loop in a background thread
-    setup_hotspot()
     app.run(host="0.0.0.0", port=5001)  # Accessible on the Pi's IP address
---
+++ user_interface/setup_hotspot.py
@@ -0,0 +1,42 @@
+import nmcli
+
+
+def setup_hotspot(password):
+    connections = nmcli.connection()
+    connections = [n.name for n in connections if n.conn_type == "wifi"]
+
+    if "RPI-Hotspot" in connections:
+        print("Hotspot already set up, returning!")
+        return
+
+    print("Setting up hotspot...")
+    nmcli.device.wifi_hotspot(
+        ssid="AirplayBridge",
+        password=password,
+        band="bg",
+        con_name="RPI-Hotspot",
+    )
+
+    nmcli.connection.modify(
+        "RPI-Hotspot",
+        {
+            "ipv4.addresses": "10.0.10.1/24",
+            "ipv4.method": "manual",
+            "ipv4.gateway": "",
+            "ipv4.dns": "",
+            "connection.autoconnect": "yes",
+            "connection.autoconnect-priority": "-1",
+        },
+    )
+
+    nmcli.connection.down("RPI-Hotspot")  # Apply new settings
+
+
+if __name__ == "__main__":
+    ans1 = 1
+    ans2 = 2
+    while ans1 != ans2:
+        ans1 = input("Password: ")
+        ans2 = input("Confirm password: ")
+
+    setup_hotspot(ans1)
